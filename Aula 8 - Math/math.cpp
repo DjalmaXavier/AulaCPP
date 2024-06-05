3cd48709128927d3b6ea43929b8cd700769715de
Djalma Xavier <djalmaxavier42@gmail.com>
2024-06-04 21:36:55 -0300

    about math

---
+++ Aula 8 - Math/math.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double x = 3.14;
+    double y = 4;
+    double z;
+
+    // z = std::max(x, y); Max number
+    // z = std::min(x, y); Min number
+    // z = pow(x, y); Power raised to the base number
+    // z = sqrt(9); Square rote;
+    // z = abs(-3); Absolute value
+    // z = round(x); round number
+    // z = ceil(x); round up
+    // z = floor(x); round down
+
+    std::cout << z;
+
+    return 0;
+}
