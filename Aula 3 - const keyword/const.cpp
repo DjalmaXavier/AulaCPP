4a5c9367881b468729bd60f25026c2b1f8d852fb
Djalma Xavier <djalmaxavier42@gmail.com>
2024-06-04 21:10:30 -0300

    const in c++

---
+++ Aula 3 - const keyword/const.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    // The const keyword specifies that a variable's value is constant (read - only)
+
+    const double PI = 3.14159;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+
+    return 0;
+}
