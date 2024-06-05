166ca1bfa972773447c15fc2fa906d0bbf3f50a3
Djalma Xavier <djalmaxavier42@gmail.com>
2024-06-04 21:09:47 -0300

    hello world commit

---
+++ Aula 1 - Hello World/helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    // This is a comment
+
+    std::cout << "Hello World!" << "\n";
+    std::cout << "This is not my first programming lang, but i love it" << "\n";
+    return 0;
+}
